b461c90d6be7c70f926e764eae3066ea88565889
Kevin Avila <kevin.avila@utexas.edu>
2016-10-07 11:33:20 -0500

    Created Xcode project.

---
+++ Kairos/KairosTests/KairosTests.swift
@@ -0,0 +1,36 @@
+//
+//  KairosTests.swift
+//  KairosTests
+//
+//  Created by Kevin Avila on 10/7/16.
+//  Copyright © 2016 BDP. All rights reserved.
+//
+
+import XCTest
+@testable import Kairos
+
+class KairosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
