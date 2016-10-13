7d6f3aa7eb86156fb6aa8bf761e429e626666c8f
Kevin Avila <kevin.avila@utexas.edu>
2016-10-12 21:40:30 -0500

    Installed FirebaseAuth pod and set up Facebook login/logout

--- Kairos/Kairos/LoginController.swift
+++ Kairos/Kairos/LoginController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FBSDKLoginKit
 
-class LoginController: UIViewController {
+class LoginController: UIViewController, FBSDKLoginButtonDelegate {
 
+    @IBOutlet weak var facebookLoginButton: FBSDKLoginButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        facebookLoginButton.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if error != nil {
+            print(error!.localizedDescription)
+            return
+        }
+        
+        let credential = FIRFacebookAuthProvider.credentialWithAccessToken(FBSDKAccessToken.currentAccessToken().tokenString)
+        FIRAuth.auth()?.signInWithCredential(credential, completion: { (user, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            }
+            print("User logged in with facebook...")
+        })
+    }
+    
+    func loginButtonWillLogin(loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        try! FIRAuth.auth()!.signOut()
+        print("User logged out of facebook...")
+    }
 
 }
 
