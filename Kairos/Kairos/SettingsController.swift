07f8834809b5214a8dd4b7be3fdf3f73724e6988
Kevin Avila <kevin.avila@utexas.edu>
2016-11-28 22:27:37 -0600

    Integrated database with settings screen.

--- Kairos/Kairos/SettingsController.swift
+++ Kairos/Kairos/SettingsController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseDatabase
+import FirebaseAuth
 
 class SettingsController: UIViewController {
     
@@ -16,13 +17,35 @@ class SettingsController: UIViewController {
     @IBOutlet weak var enableRemindersButton: UIButton!
     @IBOutlet weak var noRemindersButton: UIButton!
     var timeForReminders: Date? = nil
-    var remindersEnabled: Bool!
+    
+    var databaseRef:FIRDatabaseReference!
+    var user:FIRUser!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        enableRemindersButton.isHidden = true
-        datePicker.datePickerMode = UIDatePickerMode.time
+        let database = FIRDatabase.database()
+        databaseRef = database.reference()
+        
+        user = FIRAuth.auth()?.currentUser
+        
+        self.datePicker.datePickerMode = UIDatePickerMode.time
+        databaseRef.child(user.uid+"/reminder_time").observeSingleEvent(of: .value, with: { (snapshot) in
+            let text = snapshot.value as? String
+            if (text != nil) {
+                if (text == "REMINDERS_DISABLED") {
+                    self.noRemindersButtonTapped(UIDatePicker())
+                } else {
+                    // Set date picker to date from database
+                    self.enableRemindersButton.isHidden = true
+                }
+            } else { // first time opening settings screen
+                self.enableRemindersButtonTapped(UIDatePicker())
+            }
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
     }
     
     @IBAction func datePickerAction(_ sender: AnyObject) {
@@ -32,18 +55,24 @@ class SettingsController: UIViewController {
         dateFormatter.timeStyle = DateFormatter.Style.short
         let timeAsString = dateFormatter.string(from: timeForReminders!)
         print("Time picked: \(timeAsString)")
+        
+        databaseRef.child(user.uid+"/reminder_time").setValue(timeAsString)
     }
     
     @IBAction func noRemindersButtonTapped(_ sender: AnyObject) {
         datePicker.isHidden = true
         enableRemindersButton.isHidden = false
         noRemindersButton.isHidden = true
+        
+        databaseRef.child(user.uid+"/reminder_time").setValue("REMINDERS_DISABLED")
     }
     
     @IBAction func enableRemindersButtonTapped(_ sender: AnyObject) {
         datePicker.isHidden = false
         noRemindersButton.isHidden = false
         enableRemindersButton.isHidden = true
+        
+        datePickerAction(UIDatePicker())
     }
     
     
